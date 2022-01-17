28a41ee0dad7bd545888365a0ac6d0472e09ae0c
dante <maxgamerdu@gmail.com>
2022-01-17 06:28:22 +0300

    added my countdown pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { TimeCountPipe } from './Pipes/time-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    TimeCountPipe
   ],
   imports: [
     BrowserModule,
