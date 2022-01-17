ad39722d1f11007475dc4bc3ea4006a6fb784c6c
dante <maxgamerdu@gmail.com>
2022-01-17 15:46:47 +0300

    initialized my testing environment as well as set up a mapping system

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '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',
-  },
+    apiKey: "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",
+    authDomain: "project.firebaseapp.com",
+    databaseURL: "https://project.firebaseio.com",
+    projectId: "project",
+    storageBucket: "project.appspot.com",
+    messagingSenderId: "..."
+  }
 };
